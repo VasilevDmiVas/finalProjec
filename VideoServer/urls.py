3566d2ba5b2d516965e55b05f4e661283a68f3fe
Дмитрий <vasja8102@mail.ru>
2024-03-14 18:42:04 +0300

    финальный проект добавлен домашний шаблон

--- VideoServer/urls.py
+++ VideoServer/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('tarif/', views.tarif, name='tarif'),
+    path('voity/', views.voity, name='voity'),
+    path('zamer/', views.zamer, name='zamer'),
+    path('balans/', views.balans, name='balans'),
+    path('shop/', views.shop, name='shop'),
 ]
--- VideoServer/views.py
+++ VideoServer/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
     return render(request, 'home.html')
 
 
 def about(request):
     return render(request, 'about.html')
+
+
+def tarif(request):
+    return HttpResponse('Страница в разработке')
+
+
+def voity(request):
+    return HttpResponse('Страница в разработке')
+
+
+def zamer(request):
+    return HttpResponse('Страница в разработке')
+
+
+def balans(request):
+    return HttpResponse('Страница в разработке')
+
+
+def shop(request):
+    return HttpResponse('Страница в разработке')
